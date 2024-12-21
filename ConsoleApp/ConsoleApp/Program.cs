76e1fd326e86df7aa016d96dd69707707d5afa2c
Nastya-22-06 <nastya_22_06@mail.ru>
2024-12-21 22:26:36 +0300

    Add ConsoleApp

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace ConsoleApp
+{
+    public class SumCalculator
+    {
+        public int CalculateSum(int start, int end)
+        {
+            if (start > end)
+            {
+                throw new ArgumentException("Start cannot be greater than end.");
+            }
+
+            int sum = 0;
+            for (int i = start; i <= end; i++)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 1;
+            int b = 10;
+           
+            SumCalculator sumCalculator = new SumCalculator();
+            Console.WriteLine($"Сумма чисел от {a} до {b} = { sumCalculator.CalculateSum(a, b)}");
+        }
+    }
+}
